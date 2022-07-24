719b97c3155f971966de2dadd90fe351a3ea0970
imEckenrode <imeck121@gmail.com>
2022-07-24 11:35:23 -0400

    Added header files for all the current libraries

--- MASTER/DnDevice/main/the_functions.h
+++ MASTER/DnDevice/main/the_functions.h
@@ -1,5 +1,9 @@
-#ifndef MY_HEADER_H
-# define MY_HEADER_H
+#ifndef the_functions.h
+# define the_functions.h
+
+#include <stdbool.h>
+
+
 
 /* What is this?
 
@@ -17,3 +21,33 @@ order:      (global variables on top, then)
     Comment about what the function will do
     Function
 */
+
+// current_context tracks what state the device is in, so it cannot attack when no one is logged in
+struct context_s{
+    bool connected;         //Set to True when the device connects to the DM    (set DM to True)
+    bool loggedIn;          //Set to True when the player
+    bool onePC;             //Set to True if one character is selected 
+    bool DMHere;            //Set to True if the DM is selected (so there's no point sending ESPNow to yourself in the event of MultiPlayers)
+    //bool multiPlayers;       //Set to True if the device is hosting more than one player (which means it has to worry about routing) DM included
+} current_context = {false,false,false,false,false};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
