96027b9d8ed0b67623f76b0cdf56358040e26a7f
imEckenrode <imeck121@gmail.com>
2022-07-24 08:27:43 -0400

    Added the header for the_functions header file

--- MASTER/DnDevice/main/the_functions.h
+++ MASTER/DnDevice/main/the_functions.h
@@ -1,4 +1,19 @@
 #ifndef MY_HEADER_H
 # define MY_HEADER_H
 
-// All variables and functions for the DnDevice can be found in here
+/* What is this?
+
+ All shared varibles, function-specific structures, and function declarations for the DnDevice can be found in here
+
+
+ That means functions can be called from the screen, from comms, from console, or from wherever else
+    Just import "the_functions.h"
+
+    This will allow the usage of the_functions.c code anywhere that incluced "the_functions.h"
+
+order:      (global variables on top, then)
+    Title
+    Struct
+    Comment about what the function will do
+    Function
+*/
