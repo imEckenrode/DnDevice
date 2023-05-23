afc0405326904521dc0ff77d7401adab1b516621
imEckenrode <imeck121@gmail.com>
2023-05-23 18:52:19 -0400

    May have fixed nvs storage for the database

--- MASTER/DnDevice/main/dndv_db.c
+++ MASTER/DnDevice/main/dndv_db.c
@@ -4,10 +4,10 @@
 
 //  IDs: 1xxxxxxxxxxxxxxx, where each x is a bit
 //   Ten digits for a person (actual key), then the last 5 bytes are for the player's character
-//          The last 5 bytes must be >0, since 1xxxxxxxxxx00000 is understood as the player him/herself
+//          The last 5 bit word must be >0, since 1xxxxxxxxxx00000 is understood as the player him/herself
 // 0xx... keys are reserved for NPCs
 
-//      This could be programatically expanded to four bytes if someone has over 31 characters for some reason (or have them make a new Player)
+//If someone has more than 31 players, enter them as a new Player
 
 //The database has two namespaces:
 char* playerSpace = "People";      //Keys to People and List Of Character Names/IDs
@@ -50,29 +50,32 @@ void db_test_initialize(void){
 esp_err_t db_write_player(Key key, Player player){      //Keys are ASCII strings, technically
     nvs_handle_t handle;
     nvs_open(playerSpace, NVS_READWRITE, &handle);
-    nvs_set_blob(handle, key, player, sizeof(Player));
+    nvs_set_blob(handle, key, &player, sizeof(Player));
     nvs_close(handle);
 }
 
 esp_err_t db_write_pc(Key key, PC pc){
     nvs_handle_t handle;
     nvs_open(pcSpace, NVS_READWRITE, &handle);
-    nvs_set_blob(handle, key, pc, sizeof(PC));
+    nvs_set_blob(handle, key, &pc, sizeof(PC));
     nvs_close(handle);
 }
 
 Player db_read_player(Key key){
     nvs_handle_t handle;
     nvs_open(playerSpace, NVS_READONLY, &handle);
-    Player value = nvs_get_blob(handle, key, value, sizeof(Player));
-    nvs_close();
-    return value;
+    Player* value;
+    esp_err_t err = nvs_get_blob(handle, key, value, sizeof(Player));
+    nvs_close(handle);
+    return *value;
 }
 
 PC db_read_pc(Key key){
     nvs_handle_t handle;
-    nvs_open(playerSpace, NVS_READONLY &handle);
-    Player value = nvs_get_blob(handle, key, value, sizeof(PC));
-    nvs_close();
-    return value;
+    nvs_open(playerSpace, NVS_READONLY, &handle);
+    PC* value;
+    esp_err_t err = nvs_get_blob(handle, key, value, sizeof(PC));
+    nvs_close(handle);
+    return *value;
 }
+
