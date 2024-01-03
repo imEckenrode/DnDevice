5ade9331ab4eca7e267bf330003ae77cbb9632b5
imEckenrode <imeck121@gmail.com>
2024-01-02 21:42:23 -0500

    Transferred over ui loop, nvs init, and event loop init. Bringing in UI next

---
+++ main/device.c
@@ -0,0 +1,31 @@
+#include "device.h"
+
+#include <stdbool.h>
+#include <string.h> //for base comparison
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+void nvs_init(void){        //Needed for WiFi and the DB
+    esp_err_t ret = nvs_flash_init();   //Initialize the nvs, and use the return to clear out if more space is needed
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
+void eventLoop_init(void){
+    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+
+    esp_event_loop_args_t loop_args = {
+        .queue_size = 20,
+        .task_name = "eventLoopTask",     //A task will be created automatically 
+        .task_priority = uxTaskPriorityGet(NULL),
+        .task_stack_size = 4096,      //Up the stack size if this keeps running out of memory, but it should be fine
+        .task_core_id = tskNO_AFFINITY
+    };
+    esp_event_loop_create(&loop_args, &dndv_event_h);
+}
+
--- main/device.h
+++ main/device.h
@@ -4,8 +4,18 @@
 
 This contains all device variables and manipulations of said variables
 
-This includes key data, player name, etc
-
-For device-level data, see device_data
+This includes key data, player name, settings, etc.
 
 */
+
+
+/*      -  NVS  - 
+Initialize the non-volatile storage library to persist data 
+    Required for: ESP-NOW   */
+void nvs_init(void);
+
+//Device event handler
+esp_event_loop_handle_t dndv_event_h;
+
+//Initialize the event loop library
+void eventLoop_init(void);
